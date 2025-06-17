f4f0bc28f46b9973f20d23423d2755255513beb1
Alexey Titaev <alx.titaev@yandex.ru>
2025-06-17 22:36:31 +0300

    tests: add tests for orders repository

--- internal/repo/repo_test.go
+++ internal/repo/repo_test.go
@@ -12,6 +12,8 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/talx-hub/gopher-bonus/internal/dbmanager"
+	"github.com/talx-hub/gopher-bonus/internal/model"
+	"github.com/talx-hub/gopher-bonus/internal/model/order"
 	"github.com/talx-hub/gopher-bonus/internal/model/user"
 )
 
@@ -59,6 +61,15 @@ func TestUserRepository_Create(t *testing.T) {
 
 	exists := repo.Exists(ctx, "user1hash")
 	assert.True(t, exists)
+
+	err = repo.Create(ctx, &user.User{
+		LoginHash:    "user2hash",
+		PasswordHash: "user2password-hash",
+	})
+	require.NoError(t, err)
+
+	exists = repo.Exists(ctx, "user2hash")
+	assert.True(t, exists)
 }
 
 func TestUserRepository_FindByLogin(t *testing.T) {
@@ -101,4 +112,158 @@ func TestUserRepository_FindByID(t *testing.T) {
 	assert.Equal(t, "", u3.LoginHash)
 	assert.Equal(t, "", u3.PasswordHash)
 	assert.Equal(t, "", u3.ID)
+}
+
+func TestOrderRepository_Create(t *testing.T) {
+	repo := NewOrderRepository(pool, slog.Default())
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	err := repo.Create(ctx, &order.Order{
+		UploadedAt: time.Now(),
+		Status:     order.StatusNew,
+		ID:         "1",
+		UserID:     "1",
+	})
+	require.NoError(t, err)
+
+	err = repo.Create(ctx, &order.Order{
+		UploadedAt: time.Now(),
+		Status:     order.StatusNew,
+		ID:         "2",
+		UserID:     "1",
+	})
+	require.NoError(t, err)
+	err = repo.Create(ctx, &order.Order{
+		UploadedAt: time.Now(),
+		Status:     order.StatusNew,
+		ID:         "3",
+		UserID:     "1",
+	})
+	require.NoError(t, err)
+	err = repo.Create(ctx, &order.Order{
+		UploadedAt: time.Now(),
+		Status:     order.StatusNew,
+		ID:         "4",
+		UserID:     "1",
+	})
+	require.NoError(t, err)
+	err = repo.Create(ctx, &order.Order{
+		UploadedAt: time.Now(),
+		Status:     order.StatusNew,
+		ID:         "5",
+		UserID:     "1",
+	})
+	require.NoError(t, err)
+	err = repo.Create(ctx, &order.Order{
+		UploadedAt: time.Now(),
+		Status:     order.StatusNew,
+		ID:         "6",
+		UserID:     "2",
+	})
+	require.NoError(t, err)
+}
+
+func TestOrderRepository_FindByID(t *testing.T) {
+	repo := NewOrderRepository(pool, slog.Default())
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	o, err := repo.FindByID(ctx, "1")
+	require.NoError(t, err)
+
+	assert.Equal(t, "1", o.UserID)
+
+	o, err = repo.FindByID(ctx, "7")
+	require.Error(t, err)
+	assert.Equal(t, order.Order{}, o)
+}
+
+func TestOrderRepository_ListByUserID(t *testing.T) {
+	repo := NewOrderRepository(pool, slog.Default())
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	orders, err := repo.ListByUserID(ctx, "1")
+	require.NoError(t, err)
+	assert.Equal(t, 5, len(orders))
+	for _, o := range orders {
+		assert.Equal(t, "1", o.UserID)
+		assert.Equal(t, order.StatusNew, o.Status)
+	}
+
+	orders, err = repo.ListByUserID(ctx, "2")
+	require.NoError(t, err)
+	assert.Equal(t, 1, len(orders))
+	assert.Equal(t, "2", orders[0].UserID)
+	assert.Equal(t, order.StatusNew, orders[0].Status)
+
+	orders, err = repo.ListByUserID(ctx, "3")
+	require.NoError(t, err)
+	assert.Equal(t, 0, len(orders))
+}
+
+func TestOrderRepository_UpdateOrder(t *testing.T) {
+	repo := NewOrderRepository(pool, slog.Default())
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1000*time.Second)
+	defer cancel()
+	err := repo.UpdateOrder(ctx, &order.Order{
+		Status:  order.StatusProcessed,
+		ID:      "1",
+		Accrual: model.NewAmount(100, 5),
+	})
+	require.NoError(t, err)
+
+	err = repo.UpdateOrder(ctx, &order.Order{
+		Status:  order.StatusProcessed,
+		ID:      "2",
+		Accrual: model.NewAmount(0, 0),
+	})
+	require.NoError(t, err)
+
+	err = repo.UpdateOrder(ctx, &order.Order{
+		Status:  order.StatusProcessed,
+		ID:      "3",
+		Accrual: model.NewAmount(0, 51),
+	})
+	require.NoError(t, err)
+
+	err = repo.UpdateOrder(ctx, &order.Order{
+		Status:  order.StatusProcessed,
+		ID:      "4",
+		Accrual: model.NewAmount(100, 500),
+	})
+	require.NoError(t, err)
+
+	err = repo.UpdateOrder(ctx, &order.Order{
+		Status: order.StatusProcessing,
+		ID:     "5",
+	})
+	require.NoError(t, err)
+
+	err = repo.UpdateOrder(ctx, &order.Order{
+		Status: order.StatusProcessing,
+		ID:     "6",
+	})
+	require.NoError(t, err)
+
+	err = repo.UpdateOrder(ctx, &order.Order{
+		Status:  order.StatusProcessed,
+		ID:      "6",
+		Accrual: model.NewAmount(0, 500),
+	})
+	require.NoError(t, err)
+
+	err = repo.UpdateOrder(ctx, &order.Order{
+		Status: order.StatusInvalid,
+		ID:     "7",
+	})
+	require.Error(t, err)
+
+	err = repo.UpdateOrder(ctx, &order.Order{
+		Status: order.StatusInvalid,
+		ID:     "5",
+	})
+	require.NoError(t, err)
 }
